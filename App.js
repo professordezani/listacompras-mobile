ce287a487982a5544951206fe4bf1960aae7f073
Henrique Dezani <dezani@gmail.com>
2020-06-24 09:01:20 -0300

    Versão inicial

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [contador, setContador] = useState(0);
+
+  function incrementar() {
+    setContador(contador + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>{contador}</Text>
+      <Button title="Incrementar" onPress={incrementar}></Button>
     </View>
   );
 }
@@ -15,5 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  texto: {
+    color: '#000',
+    fontSize: 28,
+    fontWeight: 'bold'
+  }
 });
